23db7873ad176716b554cfc0ef8d666039443806
mogren0 <omaramara00@gmail.com>
2024-09-26 18:01:30 +0200

    script done 26-09-2024

--- src/main/java/org/example/DBConfig.java
+++ src/main/java/org/example/DBConfig.java
@@ -1,5 +1,10 @@
 package org.example;
-
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class DBConfig {
     String dbpath;
@@ -7,11 +12,33 @@ public class DBConfig {
     public DBConfig(String dbpath) {
         this.dbpath = dbpath;
     }
-    public static DBConfig LoadDBConfig(String fichier_config){
 
+    public String getDbpath() {
+        return dbpath;
+    }
+
+    public static DBConfig loadDBConfig(String fichier_config){
         DBConfig config = new DBConfig(fichier_config);
 
-        return config;
+        JSONParser jsonP = new JSONParser();
+        try {
+            JSONObject jsonO = (JSONObject)jsonP.parse(new FileReader(fichier_config));
+            String name = (String) jsonO.get("name");
+            String age = (String) jsonO.get("age");
+            String address = (String) jsonO.get("address");
+            System.out.println("Name :"+ name);
+            System.out.println("Age: "+ age);
+            System.out.println("Address: "+ address);
+            return config;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
 
 
     }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,8 @@
 package org.example;
-import java.io.*;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+
 public class Main {
-
-        public static void main(String[] args) {
-            JSONParser jsonP = new JSONParser();
-            try {
-                JSONObject jsonO = (JSONObject)jsonP.parse(new FileReader("C:/PROJET_BDDA/files/dataset_1.json"));
-
-                String name = (String) jsonO.get("name");
-                String age = (String) jsonO.get("age");
-                String address = (String) jsonO.get("address");
-                System.out.println("Name :"+ name);
-                System.out.println("Age: "+ age);
-                System.out.println("Address: "+ address);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-        }
+    public static void main(String[] args) {
+        DBConfig.loadDBConfig("/PROJET_BDDA/files/dataset_1.json");
+    }
 
 }
