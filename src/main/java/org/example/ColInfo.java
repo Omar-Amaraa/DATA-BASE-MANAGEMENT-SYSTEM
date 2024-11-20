ca0a60232ccb04e491e34421fd91120abf27871a
zinebfennich <penpalzineb@gmail.com>
2024-11-20 23:34:33 +0100

    Ajout des modifications de ColInfo.java avec Relation, Record et RelationTest

--- src/main/java/org/example/ColInfo.java
+++ src/main/java/org/example/ColInfo.java
@@ -1,16 +1,34 @@
 package org.example;
 
 public class ColInfo {
-    private  String nom;
-    private  String type;
-    private ColInfo(String n,String t){
-        this.nom=n;
-        this.type=t;
+    private String nom;
+    private ColType type;
+    private int tailleMax; // Utilisé pour CHAR(T) et VARCHAR(T)
+
+    public ColInfo(String nom, ColType type, int tailleMax) {
+        this.nom = nom;
+        this.type = type;
+        this.tailleMax = tailleMax;
     }
-    private String getColType(){
-        return type;
+
+    public ColInfo(String nom, ColType type) {
+        this(nom, type, 0); // Pour INT et REAL
     }
-    private String getColNom(){
+
+    public String getNom() {
         return nom;
     }
+
+    public ColType getType() {
+        return type;
+    }
+
+    public int getTailleMax() {
+        return tailleMax;
+    }
+}
+
+// Enum pour les types de colonnes
+enum ColType {
+    INT, REAL, CHAR, VARCHAR
 }
