37c399b3f921460018d4b54056e9151f30cb352f
Daphne <123645212+thiphuongnhi-chau@users.noreply.github.com>
2024-12-18 22:18:25 +0100

    rename file execute

--- execute.bat
+++
@@ -1,2 +0,0 @@
-javac -cp .\lib\json-simple-1.1.1.jar -d .\bin .\src\main\java\org\example\*.java
-java -cp .\lib\json-simple-1.1.1.jar;.\bin org.example.SGBD
--- execute.sh
+++
@@ -1,2 +0,0 @@
-javac -cp ./lib/json-simple-1.1.1.jar -d ./target/classes ./src/main/java/org/example/*.java
-java -cp ./target/classes:./lib/json-simple-1.1.1.jar org.example.SGBD
