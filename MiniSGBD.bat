597ec2b2304948c037d57f48ba5ba55783887c70
Daphne <123645212+thiphuongnhi-chau@users.noreply.github.com>
2024-12-19 02:08:55 +0100

    update execute script to run with argument

--- MiniSGBD.bat
+++ MiniSGBD.bat
@@ -1,2 +1,4 @@
+@echo off
+set arg1=%1
 javac -cp .\lib\json-simple-1.1.1.jar -d .\bin .\src\main\java\org\example\*.java
-java -cp .\lib\json-simple-1.1.1.jar;.\bin org.example.SGBD ./configDB.json
+java -cp .\lib\json-simple-1.1.1.jar;.\bin org.example.SGBD %arg1%
--- MiniSGBD.sh
+++ MiniSGBD.sh
@@ -1,2 +1,3 @@
+configDB=$1
 javac -cp ./lib/json-simple-1.1.1.jar -d ./target/classes ./src/main/java/org/example/*.java
-java -cp ./target/classes:./lib/json-simple-1.1.1.jar org.example.SGBD ./configDB.json
+java -cp ./target/classes:./lib/json-simple-1.1.1.jar org.example.SGBD $configDB
